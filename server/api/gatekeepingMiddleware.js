da1b3f731a788e98a23fa7dab2e265cdb353a924
Kelsey Siman <ksiman14@gmail.com>
2021-05-16 14:12:07 -0400

    feat: added gatekeeping middleware

---
+++ server/api/gatekeepingMiddleware.js
@@ -0,0 +1,25 @@
+const { User } = require('../db');
+
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const user = await User.findByToken(token); //the findByToken instance methodis exists in our db model
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.user.userType !== 'admin') {
+    return res.status(403).send('You shall not pass!');
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  requireToken,
+  isAdmin,
+};
